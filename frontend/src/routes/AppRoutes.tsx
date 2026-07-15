30827ff85cbc72c55f3fceb103d6ae35d3b478fb
radovoahary <voaharyradoniaina@gmail.com>
2026-07-15 14:33:04 +0300

    Begin the code...

---
+++ frontend/src/routes/AppRoutes.tsx
@@ -0,0 +1,27 @@
+import { Routes, Route } from "react-router-dom";
+
+import MainLayout from "../layouts/MainLayout";
+import HomePage from "../pages/HomePage";
+import LoginPage from "../pages/LoginPage";
+import RegisterPage from "../pages/RegisterPage";
+import NotFoundPage from "../pages/NotFoundPage";
+
+function AppRoutes() {
+  return (
+    <Routes>
+      {/* Toutes les pages utilisant le MainLayout */}
+      <Route path="/" element={<MainLayout />}>
+        <Route index element={<HomePage />} />
+      </Route>
+
+      {/* Pages sans MainLayout */}
+      <Route path="/login" element={<LoginPage />} />
+      <Route path="/register" element={<RegisterPage />} />
+
+      {/* Page 404 */}
+      <Route path="*" element={<NotFoundPage />} />
+    </Routes>
+  );
+}
+
+export default AppRoutes;
