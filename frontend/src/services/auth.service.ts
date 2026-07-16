36d02bff8cbcec3f9dabd806ce5b3446b0b1cef3
radovoahary <voaharyradoniaina@gmail.com>
2026-07-16 15:47:57 +0300

    updating authService

--- frontend/src/services/auth.service.ts
+++ frontend/src/services/auth.service.ts
@@ -1,26 +1,27 @@
-import axios from "axios";
+import api from "./api";
+
 import type {
   LoginRequest,
   RegisterRequest,
   AuthResponse,
 } from "../types/auth.types";
 
-const api = axios.create({
-  baseURL: "http://localhost:3000/api",
-});
-
-export const login = async (
-  data: LoginRequest
-): Promise<AuthResponse> => {
-  const response = await api.post("/auth/login", data);
+// Connexion
+export async function login(data: LoginRequest) {
+  const response = await api.post<AuthResponse>(
+    "/auth/login",
+    data
+  );
 
   return response.data;
-};
+}
 
-export const register = async (
-  data: RegisterRequest
-): Promise<AuthResponse> => {
-  const response = await api.post("/auth/register", data);
+// Inscription
+export async function register(data: RegisterRequest) {
+  const response = await api.post<AuthResponse>(
+    "/auth/register",
+    data
+  );
 
   return response.data;
-};
+}
