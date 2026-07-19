54c1f2ad2a0738869033422a21a3b872b374f350
radovoahary <voaharyradoniaina@gmail.com>
2026-07-19 11:40:28 +0300

    Separate Login's Page style

--- frontend/src/pages/LoginPage.tsx
+++ frontend/src/pages/LoginPage.tsx
@@ -1,86 +1,28 @@
+import AuthLayout from "../components/auth/AuthLayout";
+import LoginForm from "../components/auth/LoginForm";
 
-import { useForm } from "react-hook-form";
-import { zodResolver } from "@hookform/resolvers/zod";
-import { z } from "zod";
-import Input from "../components/ui/Input";
-import Button from "../components/ui/Button";
-
-const loginSchema = z.object({
-
-  email: z
-    .string()
-    .email("Adresse email invalide"),
-
-  // Mot de passe d'au moins 6 caractères
-  password: z
-    .string()
-    .min(6, "Minimum 6 caractères"),
-});
-
-type LoginForm = z.infer<typeof loginSchema>;
+/*
+    La page ne fait qu'assembler les composants.
+*/
 
 function LoginPage() {
 
-  // Initialisation du formulaire
-  const {
-    register,
-    handleSubmit,
-    // Contient toutes les erreurs
-    formState: { errors }
+    return (
 
-  } = useForm<LoginForm>({
-    // Utilise Zod pour valider le formulaire
-    resolver: zodResolver(loginSchema)
-  });
+        <AuthLayout>
 
-  // Fonction exécutée après validation
-  function onSubmit(data: LoginForm) {
+            <h1 className="mb-8 text-center text-3xl font-bold">
 
-    console.log(data);
+                Connexion
 
-  }
+            </h1>
 
-  return (
+            <LoginForm />
 
-    <div className="mx-auto mt-20 max-w-md">
+        </AuthLayout>
 
-      <h1 className="mb-8 text-3xl font-bold">
-        Connexion
-      </h1>
+    );
 
-      {/* Formulaire */}
-      <form
-        onSubmit={handleSubmit(onSubmit)}
-        className="space-y-5"
-      >
-        {/* Champ Email */}
-        <div>
-          <Input
-            type="email"
-            placeholder="Adresse email"
-            {...register("email")}
-          />
-          {/* Affiche l'erreur si elle existe */}
-          <p className="mt-1 text-sm text-red-500">
-            {errors.email?.message}
-          </p>
-        </div>
-        {/* Champ Mot de passe */}
-        <div>
-          <Input
-            type="password"
-            placeholder="Mot de passe"
-            {...register("password")}
-          />
-          <p className="mt-1 text-sm text-red-500">
-            {errors.password?.message}
-          </p>
-        </div>
-        <Button type="submit">
-          Se connecter
-        </Button>
-      </form>
-    </div>
-  );
 }
+
 export default LoginPage;
